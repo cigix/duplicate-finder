367c31610747d53688dbcdbe439b166de3e09304
Clément Gillard <clement.gillard@epita.fr>
2018-03-25 12:39:53 +0200

    Filter by extension ; Do not consider extension as name

--- duplicate-finder.py
+++ duplicate-finder.py
@@ -2,18 +2,25 @@
 '''Compute the difference between file names and find identical files in a
 folder hierarchy.'''
 
+import sys
+import os
+import os.path
+
 from hashlib import md5
 from levenshtein import levenshtein
 from math import sqrt
-from os import walk as explore
 
-paths = list() # {'path': path, 'hash': md5 hash, 'name': basename}
+# {'path': path, 'hash': md5 hash, 'name': basename without extension}
+paths = list()
 print('Looking for files...', end='')
 count = 0
-for path, _, files in explore('.'):
+for path, _, files in os.walk('.'):
     for file in files:
-        paths.append({'path': path + '/' + file, 'name': file})
-        count += 1
+        name, extension = os.path.splitext(file)
+        # If no extension was given, or this file's extension was given
+        if len(sys.argv) == 1 or extension in sys.argv:
+            paths.append({'path': path + '/' + file, 'name': name})
+            count += 1
 print(count, 'files found')
 
 for i, file in enumerate(paths):
@@ -27,5 +34,5 @@ for i, f1 in enumerate(paths):
     for f2 in paths[i+1:]:
         if f1['hash'] == f2['hash']:
             print(f1['path'], 'and', f2['path'], 'are identical')
-        if levenshtein(f1['name'], f2['name']) <= sqrt(len(f1['name'])):
+        elif levenshtein(f1['name'], f2['name']) <= sqrt(len(f1['name'])):
             print(f1['path'], 'and', f2['path'], 'have similar names')
