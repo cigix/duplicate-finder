b4ddd45d8dd72af44039b90fd195a6b397d7d086
cigix <cigix@cigix.me>
2021-08-28 23:14:00 +0200

    Maintenance round
    * Ignore symlinks. The point of this script is to detect where to save
      space; symlinks are easy to detect and take no space, and may be
      here already to save space.
    * Remove './' off relative paths
    * Check identical files in a separate step, only compare non-identical
      files
    * Report in a way that integrates nicely with command line tools

--- duplicate-finder.py
+++ duplicate-finder.py
@@ -22,7 +22,8 @@ paths = list()
 print("Looking for files...")
 for root, _, files in os.walk('.'):
     for file in files:
-        paths.append((root, file))
+        if not os.path.islink(os.path.join(root,file)):
+            paths.append((root, file))
 print(f"{len(paths)} files found")
 
 # Step 2: for each file, extract the name, compute the md5, and create a
@@ -30,6 +31,8 @@ print(f"{len(paths)} files found")
 def process_file(path):
     root, file = path
     filepath = os.path.join(root, file)
+    if filepath[:2] == './':
+        filepath = filepath[2:]
     with open(filepath, 'rb') as f:
         h = hashlib.md5(f.read()).digest()
     name, extension = os.path.splitext(file)
@@ -46,53 +49,70 @@ with multiprocessing.Pool() as pool:
     files = list(
             tqdm.tqdm(
                 pool.imap_unordered(process_file, paths),
-                total=len(paths)))
+                total=len(paths),
+                unit="file"))
 
 # Step 3: for each pair of files, compare their hashes, names, and resized
 # images where applicable
+hashes = dict()
+def register_hash(file):
+    hashes.setdefault(file[2], set()).add(file[0])
+
+print("\nFiltering identical files...")
+for file in files:
+    register_hash(file)
+# keep only one from the pack
+unique_files = {
+        file
+        for file in files
+        if file[0] == sorted(hashes[file[2]])[0]
+    }
+print(f"{len(files)} -> {len(unique_files)}")
+
 def compare_files(files):
     file1, file2 = files
-    # Same hash
-    identical = file1[2] == file2[2]
     # Similar name
     similar = levenshtein(file1[1], file2[2]) < math.log(len(file1[1]))
     # Similar images
     if file1[3] is not None and file2[3] is not None:
-        if identical:
-            return (file1[0], file2[0], identical, similar, True)
         with Image.open(file1[3]) as i1:
             with Image.open(file2[3]) as i2:
                 # ncc gives an unusable score when the bit depth of file 2 is
                 # less than file 1
                 # TODO: find a better compare command
                 if i1.mode != i2.mode:
-                    return (file1[0], file2[0], identical, similar, False)
+                    return (file1[0], file2[0], similar, False)
         compare = subprocess.run(
                 ['compare', '-metric', 'ncc', file1[3], file2[3], '/dev/null'],
                 capture_output=True)
         score = float(compare.stderr.decode())
-        return (file1[0], file2[0], identical, similar, 0.9 <= score <= 1.1)
-    return (file1[0], file2[0], identical, similar, False)
+        return (file1[0], file2[0], similar, 0.9 <= score <= 1.1)
+    return (file1[0], file2[0], similar, False)
 
-print("\nComparing files...")
-with multiprocessing.Pool() as pool:
-    comparisons = list(
-            tqdm.tqdm(
-                pool.imap_unordered(
-                    compare_files,
-                    itertools.combinations(files, 2)),
-                total=int(len(files)*(len(files) - 1)/2)))
+print("Comparing unique files...")
+try:
+    with multiprocessing.Pool() as pool:
+        comparisons = list(
+                tqdm.tqdm(
+                    pool.imap_unordered(
+                        compare_files,
+                        itertools.combinations(unique_files, 2)),
+                    total=int(len(unique_files)*(len(unique_files) - 1)/2),
+                    unit="files"))
+except KeyboardInterrupt:
+    comparisons = list()
 
 # Step 4: present results
 print()
-for path1, path2, identical, similar_name, similar_img in comparisons:
-    if identical:
-        print(f"{path1} and {path2} are identical")
-        continue
+for identicals in hashes.values():
+    if len(identicals) > 1:
+        print("identical:", *sorted(identicals))
+print()
+for path1, path2, similar_name, similar_img in comparisons:
     if similar_name:
-        print(f"{path1} and {path2} have similar names")
+        print(f"similar name: {path1} {path2}")
     if similar_img:
-        print(f"{path1} and {path2} have similar contents")
+        print(f"similar images: {path1} {path2}")
 
 # Step 5: clean up
 for _, _, _, tmppath in files:
