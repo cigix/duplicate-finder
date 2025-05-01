e754dca57133f19d3954adf00ef0b8b949a77a9b
cigix <cigix@cigix.me>
2025-05-01 21:58:12 +0200

    RIIR: CLI

---
+++ src/main.rs
@@ -0,0 +1,66 @@
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct CLI {
+    #[command(subcommand)]
+    mode: Mode
+}
+
+#[derive(Subcommand)]
+enum Mode {
+    /// Find and report duplicate and similar files in the current folder
+    Diff(DiffArgs),
+    /// Removes entries in the cache that do not reference a file of the current
+    /// folder
+    Clean,
+    /// Review the reported results interactively
+    Interactive
+}
+
+#[derive(Args)]
+struct DiffArgs {
+    /// Number of bits difference to have similar perceptual hash
+    diff: Option<usize>,
+    /// Number of processes to parallelize process
+    processes: Option<usize>
+}
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct CLIDefault {
+    #[command(flatten)]
+    diffargs: DiffArgs
+}
+
+fn main()
+{
+    // based on https://stackoverflow.com/a/79564853/5765334
+    let cli = CLI::try_parse()
+        // Result<CLI, Error>
+        .or_else(|err| {
+            match err.kind() {
+                clap::error::ErrorKind::InvalidSubcommand => {
+                    CLIDefault::try_parse()
+                        // Result<CLIDefault, Error>
+                        .map_or_else(
+                            |_| Err(err), // if default fails, return CLI error
+                            |cli_default| Ok(
+                                CLI { mode: Mode::Diff(cli_default.diffargs) }
+                            )
+                        )
+                        // Result<CLI, Error>
+                }
+                _ => Err(err)
+            }
+        })
+        // Result<CLI, Error>
+        .unwrap_or_else(|err| {
+            err.exit();
+        }); // CLI
+    match cli.mode {
+        Mode::Diff(_args) => todo!(),
+        Mode::Clean => todo!(),
+        Mode::Interactive => todo!()
+    }
+}
